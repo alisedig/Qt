4c594b41363a5f687ed1475578d8b46876b26453
alisedig <alisedighi02004@gmail.com>
2023-07-03 13:21:44 +0330

    add Test class

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     cout << "hello" << endl;
+    cout << "goodbye" << endl;
     MainWindow w;
     w.show();
     return a.exec();
